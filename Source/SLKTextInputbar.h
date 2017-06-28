4e7f04f59f9619b2e36090ed939212075620091d
Niall Kelly <niallkelly808@gmail.com>
2017-06-28 21:19:18 +0100

    auto hide option for left button – on by default

--- Source/SLKTextInputbar.h
+++ Source/SLKTextInputbar.h
@@ -45,6 +45,9 @@ NS_ASSUME_NONNULL_BEGIN
 /** The left action button action. */
 @property (nonatomic, strong) UIButton *leftButton;
 
+/** YES if the right button should be hidden animatedly in case the text view has no text in it. Default is YES. */
+@property (nonatomic, readwrite) BOOL autoHideLeftButton;
+
 /** The right action button action. */
 @property (nonatomic, strong) UIButton *rightButton;
 
